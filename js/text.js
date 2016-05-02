e0c53aaaf76f4a436ff03bdcaa4b39f4c16dc25a
scraft1 <sccraft90@yahoo.com>
2016-05-02 09:43:52 -0500

    change font size while typing

--- js/text.js
+++ js/text.js
@@ -5,43 +5,48 @@
 var fontSize = 40, // default
     typing = false,
     textCoord = {},
-    size,
-    text,
-    textId = 'text-area';
+    text;
+
+function setFontSize(size){
+    fontSize = parseInt(size); 
+    if(text) fontSizePosition(); 
+}
+
+function fontSizePosition(){
+    text.style.fontSize = fontSize+'px';
+    text.style.top = (textCoord.y-fontSize/2)+'px'; 
+    ctx.font=fontSize+'px sans-serif'; 
+}
 
 function createText(e){
     if(typing)drawText();
-    size = document.getElementById('font-size').value; 
-    var adj = size/10; 
-    if(size < 34)adj = Math.floor(adj);
-    else adj = Math.ceil(adj); 
-    textCoord = {x:e.offsetX+2,y:e.offsetY+size/2-adj};
-    
+    textCoord = {x:e.offsetX,y:e.offsetY}; 
     text = document.createElement('textarea');  
-    text.id = textId; 
     text.style = 'position:absolute;border:none;outline:none;resize:none';
     text.style.backgroundColor = 'transparent';
-    text.style.left = (e.offsetX)+'px';
-    text.style.top = (e.offsetY-size/2)+'px'; 
     text.style.width = width-e.offsetX+'px';
     text.style.height = height-e.offsetY+'px';
-    text.style.fontSize = size+'px';
-    text.style.lineHeight = 1; 
-    ctx.font=size+'px sans-serif'; 
+    text.style.lineHeight = 1; // needed ? 
     text.style.color = activeColor; 
+    text.style.left = (textCoord.x)+'px';
+    fontSizePosition(); 
     document.body.appendChild(text); 
     text.focus(); 
     typing = true;
 }
 
 function drawText(){
-    var lines = text.value.split('\n');
-    var y = textCoord.y; 
+    var adj = fontSize/10; 
+    if(fontSize < 34)adj = Math.floor(adj);
+    else adj = Math.ceil(adj); 
+    var x = textCoord.x+2; 
+    var y = textCoord.y+fontSize/2-adj;
+
+    var lines = text.value.split('\n'); 
     for(var i = 0; i < lines.length; i++){
-        ctx.fillText(lines[i],textCoord.x,y);
-        y += parseInt(size); 
+        ctx.fillText(lines[i],x,y);
+        y += fontSize; 
     }
-    
     text.remove(); 
     setImage(); 
     typing = false; 
